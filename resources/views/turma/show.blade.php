b3134ab530ddd3ccaadbbba15a562123891d022c
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-02-26 16:24:31 -0300

    Proteção do botão de finalização de turma

--- resources/views/turma/show.blade.php
+++ resources/views/turma/show.blade.php
@@ -85,7 +85,9 @@
 
                 <div class="text-center">
                     <a class="btn btn-ufop" role="button" href="#"><i class="fa fa-pencil-square-o"></i> Gerenciar Faltas</a>
-                    <a class="btn bg-black" role="button" href="#"><i class="fa fa-lock"></i> Finalizar Turma</a>
+                    @if(!$turma->finalizada)
+                        <a class="btn bg-black" role="button" href="#"><i class="fa fa-lock"></i> Finalizar Turma</a>
+                    @endif
                 </div>
             </div>
         </div>
