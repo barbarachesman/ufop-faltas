f1f15965ab0520938865005d0f9168e5d645b4d6
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-02-26 17:24:21 -0300

    Adicionados os campos Mass Assignable

--- app/Falta.php
+++ app/Falta.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Falta extends Model
 {
     public $timestamps = false;
+
+    protected $fillable = [
+        'aluno_id', 'turma_id', 'data',
+    ];
 }
