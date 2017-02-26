ea0f32fcd2af62f5ef7aad8fc87e73dc6289befb
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-02-26 17:27:46 -0300

    Adicionadas as relações

--- app/Falta.php
+++ app/Falta.php
@@ -11,4 +11,22 @@ class Falta extends Model
     protected $fillable = [
         'aluno_id', 'turma_id', 'data',
     ];
+
+    /**
+     * Recupera o aluno que faltou.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function aluno()
+    {
+        return $this->belongsTo('App\Usuario', 'aluno_id');
+    }
+
+    /**
+     * Recupera a turma em que ocorreu a falta
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function turma()
+    {
+        return $this->belongsTo('App\Turma');
+    }
 }
