c0eeba1cc4eb86c9f5b51a37a66c24ed06ac552e
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-02-26 17:22:23 -0300

    Adicionada a funcionalidade de desmatricular um aluno de uma turma

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Turma;
 use App\Usuario;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AlunoController extends Controller
 {
+    /**
+     * Desmatricula um aluno de uma determinada turma, apagando todas as suas faltas
+     * @param Usuario $aluno Instância do aluno
+     * @param Turma $turma Instância da turma
+     * @return \Illuminate\Http\RedirectResponse Página anterior
+     */
     public function desmatricular(Usuario $aluno, Turma $turma)
     {
-        dd("teste");
+        /* Como o Eloquent não dá suporte a chave compostas, é necessário usar a classe DB para excluir 'manualmente' */
+
+        $faltasDoAluno = DB::table('faltas')->where('aluno_id', $aluno->id)->where('turma_id', $turma->id)->delete();
+        $desmatriculado = DB::table('matriculados')->where('aluno_id', $aluno->id)->where('turma_id', $turma->id)->delete();
+
+        return redirect()->route('detalheTurma', $turma->id);
     }
 }
