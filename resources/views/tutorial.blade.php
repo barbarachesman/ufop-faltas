09ae5f7d216c4b7787a7f8ad26c0a9fd33c4a08a
Barbara <chesman12@gmail.com>
2017-03-08 19:56:09 -0300

    Estrutura tutorial

--- resources/views/tutorial.blade.php
+++ resources/views/tutorial.blade.php
@@ -5,285 +5,141 @@
 @endsection
 
 @section('descricao')
-    Neste tutorial você aprenderá a gerenciar seu diário de classe através de uma aplicação web simples e funcional: seu diário online.
+    Este tutorial fornece o passo a passo para a utilização do Diário.
+    É relevante que você o leia e veja as instruções para que possa rapidamente familiarizar-se com a forma de manipulação dos recursos disponíveis.
 @endsection
 
 @section('conteudo')
+
+
 <ul class="timeline">
           <!-- timeline time label -->
           <li class="time-label">
                 <span class="bg-gray">
-                  Recursos Professor
+                  Acesso Professor - Recursos permitidos somente para docentes
                 </span>
           </li>
-          <!-- /.timeline-label -->
-          <!-- timeline item -->
           <li>
-            <i class="fa fa-envelope bg-blue"></i>
+            <i class="fa fa-plus bg-green"></i>
 
             <div class="timeline-item">
-              <span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
 
-              <h3 class="timeline-header"><a href="#">Criar Turma</a> Importar arquivo .csv</h3>
+              <h3 class="timeline-header"><a href="#">Criar Turma</a> Minhas Turmas > Criar Turma</h3>
 
               <div class="timeline-body">
-                Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
-                weebly ning heekya handango imeem plugg dopplr jibjab, movity
-                jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
-                quora plaxo ideeli hulu weebly balihoo...
-              </div>
-              <div class="timeline-footer">
-                <a class="btn btn-primary btn-xs">Read more</a>
-                <a class="btn btn-danger btn-xs">Delete</a>
+                A partir da fase de ajuste de matrícula, os professores tem acesso ao diário das turmas que ministrarão. Portanto, com o diário de classe de extensão (.csv), o professor escolherá o arquivo no seu dispositivo e importará para a plataforma através do botão Importar.
+                Assim, a turma estará criada e pronta para o gerenciamento de faltas.
               </div>
             </div>
           </li>
-          <!-- END timeline item -->
-          <!-- timeline item -->
           <li>
-            <i class="fa fa-user bg-gray"></i>
-
+            <i class="fa fa-th-list bg-yellow"></i>
             <div class="timeline-item">
-              <span class="time"><i class="fa fa-clock-o"></i> 5 mins ago</span>
-
-              <h3 class="timeline-header no-border"><a href="#">Listar Turmas</a> accepted your friend request</h3>
+              <h3 class="timeline-header no-border"><a href="#">Listar Turmas</a> Minhas Turmas > Listar Turmas</h3>
+              <div class="timeline-body">
+                Cadastrada as turmas, o professor visualizará todas as turmas as quais ele é responsável.
+                Podendo obter detalhes de cada uma delas ou visualizar o diário.
+              </div>
             </div>
           </li>
-          <!-- END timeline item -->
-          <!-- timeline item -->
           <li>
-            <i class="fa fa-comments bg-ufop"></i>
+            <i class="fa fa-group bg-red"></i>
 
             <div class="timeline-item">
-              <span class="time"><i class="fa fa-clock-o"></i> 27 mins ago</span>
 
-              <h3 class="timeline-header"><a href="#">Gerenciar Turmas</a> commented on your post</h3>
+              <h3 class="timeline-header"><a href="#">Gerenciar Turmas</a> Minhas Turmas > Listar Turmas > Detalhes </h3>
 
               <div class="timeline-body">
                 Take me to your leader!
                 Switzerland is small and neutral!
                 We are more like Germany, ambitious and misunderstood!
               </div>
-              <div class="timeline-footer">
-                <a class="btn btn-warning btn-flat btn-xs">View comment</a>
-              </div>
             </div>
           </li>
 
           <li>
-            <i class="fa fa-comments bg-gray"></i>
+            <i class="fa fa-hand-paper-o bg-blue"></i>
 
             <div class="timeline-item">
-              <span class="time"><i class="fa fa-clock-o"></i> 27 mins ago</span>
 
-              <h3 class="timeline-header"><a href="#">Gerenciar Faltas</a> commented on your post</h3>
+              <h3 class="timeline-header"><a href="#">Gerenciar Faltas</a> Minhas Turmas > Listar Turmas > Visualizar Diário > Gerenciar Faltas</h3>
 
               <div class="timeline-body">
                 Take me to your leader!
                 Switzerland is small and neutral!
                 We are more like Germany, ambitious and misunderstood!
               </div>
-              <div class="timeline-footer">
-                <a class="btn btn-warning btn-flat btn-xs">View comment</a>
-              </div>
             </div>
           </li>
 
           <li>
-            <i class="fa fa-comments bg-yellow"></i>
+            <i class="fa fa-calendar  bg-yellow"></i>
 
             <div class="timeline-item">
-              <span class="time"><i class="fa fa-clock-o"></i> 27 mins ago</span>
 
-              <h3 class="timeline-header"><a href="#">Visualizar Diário</a> commented on your post</h3>
+              <h3 class="timeline-header"><a href="#">Visualizar Diário</a> Minhas Turmas > Listar Turmas > Visualizar Diário</h3>
 
               <div class="timeline-body">
                 Take me to your leader!
                 Switzerland is small and neutral!
                 We are more like Germany, ambitious and misunderstood!
               </div>
-              <div class="timeline-footer">
-                <a class="btn btn-warning btn-flat btn-xs">View comment</a>
-              </div>
             </div>
           </li>
-          <!-- END timeline item -->
-          <!-- timeline time label -->
           <li class="time-label">
-                <span class="bg-red">
-                  Aluno
+                <span class="bg-gray">
+                  Acesso Aluno - Recursos permitidos para discentes
                 </span>
           </li>
-          <!-- /.timeline-label -->
-          <!-- timeline item -->
           <li>
-            <i class="fa fa-camera bg-purple"></i>
+            <i class="fa fa-th-list bg-blue"></i>
 
             <div class="timeline-item">
-              <span class="time"><i class="fa fa-clock-o"></i> 2 days ago</span>
 
-              <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
+              <h3 class="timeline-header"><a href="#">Listar Turmas</a> uploaded new photos</h3>
 
               <div class="timeline-body">
-                <img src="http://placehold.it/150x100" alt="..." class="margin">
-                <img src="http://placehold.it/150x100" alt="..." class="margin">
                 <img src="http://placehold.it/150x100" alt="..." class="margin">
                 <img src="http://placehold.it/150x100" alt="..." class="margin">
               </div>
             </div>
           </li>
-          <!-- END timeline item -->
-          <!-- timeline item -->
           <li>
-            <i class="fa fa-video-camera bg-maroon"></i>
+            <i class="fa fa-calendar bg-maroon"></i>
 
             <div class="timeline-item">
-              <span class="time"><i class="fa fa-clock-o"></i> 5 days ago</span>
 
-              <h3 class="timeline-header"><a href="#">Mr. Doe</a> shared a video</h3>
+              <h3 class="timeline-header"><a href="#">Visualizar Faltas</a> shared a video</h3>
 
               <div class="timeline-body">
-                <div class="embed-responsive embed-responsive-16by9">
-                  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/tMWkeBIohBs" frameborder="0" allowfullscreen></iframe>
-                </div>
-              </div>
-              <div class="timeline-footer">
-                <a href="#" class="btn btn-xs bg-maroon">See comments</a>
+                aijoidjas
               </div>
             </div>
           </li>
-          <!-- END timeline item -->
+          <li class="time-label">
+                <span class="bg-gray">
+                  Acesso Aluno - Recursos permitidos para discentes
+                </span>
+          </li>
           <li>
-            <i class="fa fa-clock-o bg-gray"></i>
+            <i class="fa fa-th-list bg-blue"></i>
+
+            <div class="timeline-item">
+
+              <h3 class="timeline-header"><a href="#">Video explicativo</a> uploaded new photos</h3>
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/tMWkeBIohBs" frameborder="0" allowfullscreen></iframe>
+              </div>
+            </div>
           </li>
         </ul>
-      </div>
-      <!-- /.col -->
-    </div>
-    <!-- /.row -->
 
 
-<!-- /.content-wrapper -->
+        Como é um sistema pioneiro no controle de faltas, assim como todo processo novo, ele certamente estará
+        em constante ajuste até sua consolidação final, requerendo, portanto, muita PACIÊNCIA de todos os
+        envolvidos, docentes e alunos.
+        Manteremos todos informados à medida que novas ações forem necessárias.
+        As dúvidas e sugestões devem ser direcionadas por e-mail a chesman12@gmail.com.
 
-<footer class="main-footer">
-  <div class="pull-right hidden-xs">
-    <b>Version</b> 2.3.7
-  </div>
-  <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
-  reserved.
-</footer>
-
-<!-- Control Sidebar -->
-<aside class="control-sidebar control-sidebar-dark">
-  <!-- Create the tabs -->
-  <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-    <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-
-    <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-  </ul>
-  <!-- Tab panes -->
-  <div class="tab-content">
-    <!-- Home tab content -->
-    <div class="tab-pane" id="control-sidebar-home-tab">
-      <h3 class="control-sidebar-heading">Recent Activity</h3>
-      <ul class="control-sidebar-menu">
-        <li>
-          <a href="javascript:void(0)">
-            <i class="menu-icon fa fa-birthday-cake bg-red"></i>
-
-            <div class="menu-info">
-              <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-
-              <p>Will be 23 on April 24th</p>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="javascript:void(0)">
-            <i class="menu-icon fa fa-user bg-yellow"></i>
-
-            <div class="menu-info">
-              <h4 class="control-sidebar-subheading">Frodo Updated His Profile</h4>
-
-              <p>New phone +1(800)555-1234</p>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="javascript:void(0)">
-            <i class="menu-icon fa fa-envelope-o bg-light-blue"></i>
-
-            <div class="menu-info">
-              <h4 class="control-sidebar-subheading">Nora Joined Mailing List</h4>
-
-              <p>nora@example.com</p>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="javascript:void(0)">
-            <i class="menu-icon fa fa-file-code-o bg-green"></i>
-
-            <div class="menu-info">
-              <h4 class="control-sidebar-subheading">Cron Job 254 Executed</h4>
-
-              <p>Execution time 5 seconds</p>
-            </div>
-          </a>
-        </li>
-      </ul>
-      <!-- /.control-sidebar-menu -->
-
-      <h3 class="control-sidebar-heading">Tasks Progress</h3>
-      <ul class="control-sidebar-menu">
-        <li>
-          <a href="javascript:void(0)">
-            <h4 class="control-sidebar-subheading">
-              Custom Template Design
-              <span class="label label-danger pull-right">70%</span>
-            </h4>
-
-            <div class="progress progress-xxs">
-              <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="javascript:void(0)">
-            <h4 class="control-sidebar-subheading">
-              Update Resume
-              <span class="label label-success pull-right">95%</span>
-            </h4>
-
-            <div class="progress progress-xxs">
-              <div class="progress-bar progress-bar-success" style="width: 95%"></div>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="javascript:void(0)">
-            <h4 class="control-sidebar-subheading">
-              Laravel Integration
-              <span class="label label-warning pull-right">50%</span>
-            </h4>
-
-            <div class="progress progress-xxs">
-              <div class="progress-bar progress-bar-warning" style="width: 50%"></div>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="javascript:void(0)">
-            <h4 class="control-sidebar-subheading">
-              Back End Framework
-              <span class="label label-primary pull-right">68%</span>
-            </h4>
-
-            <div class="progress progress-xxs">
-              <div class="progress-bar progress-bar-primary" style="width: 68%"></div>
-            </div>
-          </a>
-        </li>
-      </ul>
-Este tutorial é mantido por Bárbara Chesman.
+        Este tutorial é mantido por Bárbara Chesman.
 @endsection
