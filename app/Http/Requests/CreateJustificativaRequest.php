cdb8045cc4bb0ab47c0213b97c2b23c5ecb59625
root <chesman12@gmail.com>
2017-08-21 23:48:45 -0300

    Justificativa faltas

--- app/Http/Requests/CreateJustificativaRequest.php
+++ app/Http/Requests/CreateJustificativaRequest.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
-class CreateAbonoRequest extends FormRequest
+class CreateJustificativaRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
